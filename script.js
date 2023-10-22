418596bf8e98c7f9c43ad690b5119ef369fcb8c6
Penny-21 <vpennacchio02@gmail.com>
2023-10-22 14:24:29 +0800

    First changes and new assets

---
+++ script.js
@@ -0,0 +1,54 @@
+// Sample data for products
+const products = [
+    {
+        name: 'T-Shirt',
+        price: '$20',
+        image: 'assets/tshirt.jpg',
+        description: 'A comfortable and stylish t-shirt.'
+    },
+    {
+        name: 'Jeans',
+        price: '$40',
+        image: 'assets/jeans.jpg',
+        description: 'Classic blue jeans for any occasion.'
+    },
+    // Add more products here
+];
+
+
+// Function to create product cards
+function createProductCard(product) {
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    
+    const image = document.createElement('img');
+    image.src = product.image;
+    
+    const name = document.createElement('h2');
+    name.textContent = product.name;
+    
+    const price = document.createElement('p');
+    price.textContent = `Price: ${product.price}`;
+    
+    const description = document.createElement('p');
+    description.textContent = product.description;
+
+    //Add a click event listener to each product card
+    productCard.addEventListener('click', () => {
+        alert(`Clicked on ${product.name}`);
+    });
+    
+    productCard.appendChild(image);
+    productCard.appendChild(name);
+    productCard.appendChild(price);
+    productCard.appendChild(description);
+    
+    return productCard;
+}
+
+// Add product cards to the page
+const productsSection = document.getElementById('products');
+products.forEach(product => {
+    const productCard = createProductCard(product);
+    productsSection.appendChild(productCard);
+});
