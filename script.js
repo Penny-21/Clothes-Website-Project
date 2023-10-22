2e4e7934c7fd8269860a1cc2cbc002193d73a955
Penny-21 <vpennacchio02@gmail.com>
2023-10-22 17:55:01 +0800

    Adjusted code and imgs

--- script.js
+++ script.js
@@ -1,54 +1,80 @@
+const backToTopLink = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    backToTopLink.style.display = "block";
+  } else {
+    backToTopLink.style.display = "none";
+  }
+});
+
+backToTopLink.addEventListener("click", (event) => {
+  event.preventDefault();
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
 // Sample data for products
 const products = [
-    {
-        name: 'T-Shirt',
-        price: '$20',
-        image: 'assets/tshirt.jpg',
-        description: 'A comfortable and stylish t-shirt.'
-    },
-    {
-        name: 'Jeans',
-        price: '$40',
-        image: 'assets/jeans.jpg',
-        description: 'Classic blue jeans for any occasion.'
-    },
-    // Add more products here
+  {
+    name: "T-Shirt",
+    price: "$20",
+    image: "assets/tshirt.jpg",
+    description: "A comfortable and stylish t-shirt.",
+  },
+  {
+    name: "Jeans",
+    price: "$40",
+    image: "assets/jeans.jpg",
+    description: "Classic blue jeans for any occasion.",
+  },
+  {
+    name: "Jeans",
+    price: "$40",
+    image: "assets/jeans.jpg",
+    description: "Classic blue jeans for any occasion.",
+  },
+  {
+    name: "T-Shirt",
+    price: "$20",
+    image: "assets/tshirt.jpg",
+    description: "A comfortable and stylish t-shirt.",
+  },
+  // Add more products here
 ];
-
 
 // Function to create product cards
 function createProductCard(product) {
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
-    
-    const image = document.createElement('img');
-    image.src = product.image;
-    
-    const name = document.createElement('h2');
-    name.textContent = product.name;
-    
-    const price = document.createElement('p');
-    price.textContent = `Price: ${product.price}`;
-    
-    const description = document.createElement('p');
-    description.textContent = product.description;
+  const productCard = document.createElement("div");
+  productCard.classList.add("product-card");
 
-    //Add a click event listener to each product card
-    productCard.addEventListener('click', () => {
-        alert(`Clicked on ${product.name}`);
-    });
-    
-    productCard.appendChild(image);
-    productCard.appendChild(name);
-    productCard.appendChild(price);
-    productCard.appendChild(description);
-    
-    return productCard;
+  const image = document.createElement("img");
+  image.src = product.image;
+
+  const name = document.createElement("h2");
+  name.textContent = product.name;
+
+  const price = document.createElement("p");
+  price.textContent = `Price: ${product.price}`;
+
+  const description = document.createElement("p");
+  description.textContent = product.description;
+
+  //Add a click event listener to each product card
+  productCard.addEventListener("click", () => {
+    alert(`Clicked on ${product.name}`);
+  });
+
+  productCard.appendChild(image);
+  productCard.appendChild(name);
+  productCard.appendChild(price);
+  productCard.appendChild(description);
+
+  return productCard;
 }
 
 // Add product cards to the page
-const productsSection = document.getElementById('products');
-products.forEach(product => {
-    const productCard = createProductCard(product);
-    productsSection.appendChild(productCard);
+const productsSection = document.getElementById("products");
+products.forEach((product) => {
+  const productCard = createProductCard(product);
+  productsSection.appendChild(productCard);
 });
