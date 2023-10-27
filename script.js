34a693b61fa0d02bb72c6c9c4d77d1eb7ae25765
Penny-21 <vpennacchio02@gmail.com>
2023-10-27 18:45:30 +0800

    video section + new imgs

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const backToTopLink = document.querySelector(".back-to-top");
 
 window.addEventListener("scroll", () => {
@@ -13,77 +14,47 @@ backToTopLink.addEventListener("click", (event) => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-
-
-
-
-
-
 // Translations for English
 const translationsEN = {
-  'menu.home': 'Home',
-  'menu.about': 'About',
-  'menu.blogs': 'Blogs',
-  'menu.portfolio': 'Portfolio',
-  'menu.contact': 'Contact',
-  'header.welcome': 'Welcome to our Clothing Store',
+  "menu.home": "Home",
+  "menu.about": "About",
+  "menu.blogs": "Blogs",
+  "menu.portfolio": "Portfolio",
+  "menu.contact": "Contact",
+  "header.welcome": "Welcome to our Clothing Store",
 };
-
-
-
-
-
-
-
-
 
 // Translations for Portuguese
 const translationsPT = {
-  'menu.home': 'Início',
-  'menu.about': 'Sobre',
-  'menu.blogs': 'Blogs',
-  'menu.portfolio': 'Portfólio',
-  'menu.contact': 'Contato',
-  'header.welcome': 'Bem-vindo à nossa loja de roupas',
+  "menu.home": "Início",
+  "menu.about": "Sobre",
+  "menu.blogs": "Blogs",
+  "menu.portfolio": "Portfólio",
+  "menu.contact": "Contato",
+  "header.welcome": "Bem-vindo à nossa loja de roupas",
 };
 
-
-
-
-
-
-
-
-
 // Define language switch button and flag image
-const languageSwitchButton = document.getElementById('language-switch');
-const flagImage = document.getElementById('flag');
-
-
-
-
-
-
-
+const languageSwitchButton = document.getElementById("language-switch");
+const flagImage = document.getElementById("flag");
 
 const languages = {
   en: {
-      flag: 'assets/uk-flag-icon.png',
-      text: 'PT',
+    flag: "assets/uk-flag-icon.png",
+    text: "PT",
   },
   pt: {
-      flag: 'assets/bandeira-nacional.jpg',
-      text: 'EN',
+    flag: "assets/bandeira-nacional.jpg",
+    text: "EN",
   },
 };
 
-let currentLanguage = 'pt'; // Default language is portuguese
+let currentLanguage = "pt"; // Default language is portuguese
 
 // Function to switch the language
 function switchLanguage() {
   const currentLanguage = document.documentElement.lang;
-  const newLanguage = currentLanguage === 'en' ? 'pt' : 'en';
-
+  const newLanguage = currentLanguage === "en" ? "pt" : "en";
 
   // Update the flag image and button text
   flagImage.src = languages[currentLanguage].flag;
@@ -93,50 +64,54 @@ function switchLanguage() {
   document.documentElement.lang = newLanguage;
 
   // Get all elements with data-translation attribute
-  const elementsToTranslate = document.querySelectorAll('[data-translation]');
+  const elementsToTranslate = document.querySelectorAll("[data-translation]");
 
   // Iterate through elements and update their content
   elementsToTranslate.forEach((element) => {
-      const translationKey = element.getAttribute('data-translation');
-      element.textContent = newLanguage === 'en' ? translationsEN[translationKey] : translationsPT[translationKey];
+    const translationKey = element.getAttribute("data-translation");
+    element.textContent =
+      newLanguage === "en"
+        ? translationsEN[translationKey]
+        : translationsPT[translationKey];
   });
 }
 
 // Event listener for the language switch button
-document.getElementById('language-switch').addEventListener('click', switchLanguage);
+document
+  .getElementById("language-switch")
+  .addEventListener("click", switchLanguage);
 
 // Initialize the page with the default language (PT)
 switchLanguage();
 
-
-
-
-
 // Sample data for products
 const products = [
   {
+    name: "Dress",
+    price: "$50",
+    image: "assets/Giuly-1.png",
+    description:
+      "Vento vem me trazer boas novas! E a novidade dessa coleção da ogue é esse chemise com essa estamapa linda de animal print!! já garante a sua, porque são poucas peças hein.",
+  },
+  {
+    name: "Body-Dress",
+    price: "$40",
+    image: "assets/Body-Dress-2.png",
+    description:
+      "Vermelho significa paixão, energia e excitação. É uma cor quente. É a cor do elemento fogo, do sangue e do coração humano. Simboliza a chama que mantém vivo o desejo, a excitação sexual e representa os sentimentos de amor e paixão.",
+  },
+  {
+    name: "Dress",
+    price: "$40",
+    image: "assets/Black-dress.png",
+    description: "O nosso Vestido Moletim é a peça que não pode faltar na sua casa para você curtir os fins de semana de uma forma mais leve e cheia de elegância!",
+  },
+  {
     name: "T-Shirt",
     price: "$20",
-    image: "assets/tshirt.jpg",
-    description: "A comfortable and stylish t-shirt.",
-  },
-  {
-    name: "Jeans",
-    price: "$40",
-    image: "assets/jeans.jpg",
-    description: "Classic blue jeans for any occasion.",
-  },
-  {
-    name: "Jeans",
-    price: "$40",
-    image: "assets/jeans.jpg",
-    description: "Classic blue jeans for any occasion.",
-  },
-  {
-    name: "T-Shirt",
-    price: "$20",
-    image: "assets/tshirt.jpg",
-    description: "A comfortable and stylish t-shirt.",
+    image: "assets/Giorgio-brasiliano.png",
+    description:
+      "Com recorte na lateral e com tecido de Moletim, o nosso Camisão assimétrico é a peça perfeita para essa época do ano, te proporcionando elegância e um conforto incrível para tirar aquela foto em família!",
   },
   // Add more products here
 ];
@@ -194,3 +169,34 @@ function showSlides() {
   slides[slideIndex - 1].style.display = "block";
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
+
+
+
+
+
+
+
+// Array of background colors to cycle through
+const colors = ['#f0f0f0', '#ffcc66', '#66ccff', '#99ff99', '#ff6666', '#ccccff'];
+
+// Function to change the background color
+function changeBackgroundColor() {
+    // Get the body element
+    const body = document.body;
+
+    // Get the current background color
+    const currentColor = body.style.backgroundColor;
+
+    // Find the index of the current color in the array
+    const currentIndex = colors.indexOf(currentColor);
+
+    // Calculate the index of the next color
+    const nextIndex = (currentIndex + 1) % colors.length;
+
+    // Set the next background color
+    body.style.backgroundColor = colors[nextIndex];
+}
+
+// Set an interval to change the background color automatically every 3 seconds (3000 milliseconds)
+setInterval(changeBackgroundColor, 3000);
+
